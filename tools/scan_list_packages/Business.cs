df8d0e2d2fc65fad66fb257d39111e3f179d3bb6
Nhu Tuan Anh <nhutuananh@gmail.com>
2017-07-29 01:58:18 +0700

    add tool to scan package

---
+++ tools/scan_list_packages/Business.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+
+namespace scan_list_packages
+{
+    internal class Business
+    {
+        private string getFileContainPackage(string dirTagFile, string package_name)
+        {
+            //string sourceDir = @"d:\Projects\4.slackware_tagfile\13.37_minimum\";
+            string[] dirEntries = Directory.GetDirectories(dirTagFile);
+            foreach (var dirName in dirEntries)
+            {
+                string[] fileEntries = Directory.GetFiles(dirName);
+                foreach (var fileName in fileEntries)
+                {
+                    //Console.WriteLine(fileName);
+                    string[] lines = File.ReadAllLines(fileName);
+                    foreach (var line in lines)
+                    {
+                        if (line.Contains(package_name))
+                            return fileName;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void skip_all_package(string dirTagFile)
+        {
+            string[] dirEntries = Directory.GetDirectories(dirTagFile);
+            foreach (var dirName in dirEntries)
+            {
+                string[] fileEntries = Directory.GetFiles(dirName);
+                foreach (var fileName in fileEntries)
+                {
+                    string text = File.ReadAllText(fileName);
+                    text = text.Replace(":ADD", ":SKP");
+                    File.WriteAllText(fileName, text);
+                }
+            }
+        }
+
+        private void add_package(string fileName, string package)
+        {
+            Console.WriteLine("fileName" + fileName);
+            if (string.IsNullOrEmpty(fileName))
+                return;
+
+            string text = File.ReadAllText(fileName);
+            if (text != null)
+            {
+                text = text.Replace(package+":SKP", package+":ADD");
+                File.WriteAllText(fileName, text);
+            }
+        }
+        
+        public void add_pkgs_from_file_to_tagdir(string fileName, string tagDir)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            foreach (var line in lines)
+            {
+                string tagFile = getFileContainPackage(tagDir, line);
+                if (string.IsNullOrEmpty(tagFile))
+                {
+                    Console.WriteLine("not found package: " + line);
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("package: " + line);
+                }
+                // update tagfile from :SKP to :ADD
+                add_package(tagFile, line);
+            }
+        }
+    }
+
+}
---
+++ tools/scan_list_packages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace scan_list_packages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //string[] lines = File.ReadAllLines(@"d:\Projects\4.slackware_tagfile\tools\scan_list_packages\list_packages");
+            // foreach (var line in lines)
+            // {
+            //     Console.WriteLine(line);
+            // }
+
+            string fileName_listPackage = @"d:\Projects\4.slackware_tagfile\tools\scan_list_packages\list_packages";
+            string tagDir = @"d:\Projects\4.slackware_tagfile\13.37_minimum\";
+            Business bu = new Business();
+            //bu.skip_all_package(@"d:\Projects\4.slackware_tagfile\13.37_minimum\");
+            bu.add_pkgs_from_file_to_tagdir(fileName_listPackage, tagDir);
+        }
+
+    }
+}
